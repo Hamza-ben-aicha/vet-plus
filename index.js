1b67df42e4701032ab534f4647de73f37cb922fc
Hamza-ben-aicha <75418750+Hamza-ben-aicha@users.noreply.github.com>
2021-12-26 02:22:48 +0100

    second commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import postRoutes from "./routes/posts.js";
 import dotenv from 'dotenv';
 
 const app = express();
-dotenv.config;
+dotenv.config();
 
 
 app.use(express.json({ limit: "30mb", extended: true }));
@@ -20,7 +20,8 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL).then(()=>{console.log('...')})
+mongoose.connect(process.env.CONNECTION_URL)
+  .then(()=>{console.log('...')})
   .then(() =>app.listen(PORT, () => console.log(`server running on port : ${PORT}`)))
   .catch((error) => console.log(error.message));
 
